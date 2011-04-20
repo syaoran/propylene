b7dccbbee4c1565f04ee0b3cb20370123bbf82a2
loris <loris@kant.(none)>
2011-04-20 17:04:18 +0200

    towards indentation and creation of a Parser object

--- propylene/propylene.py
+++ propylene/propylene.py
@@ -8,103 +8,97 @@
 import sys
 import ply.lex as lex
 import ply.yacc as yacc
-import tokenRules
+from PropLexer import PropLexer as PropLexer
 
-from tokenRules import tokens
+## from tokenRules import tokens
 from p_ast import *
 from exception import *
 
-plan_count = -1
+class Propylene:
+    def __init__ (self, *args, **kwargs):
+        self.plan_count = -1
 
-sym_table_stack = [ {}]
-variable_status = "declaration"
-#sym_table = {}
+        self.sym_table_stack = [ {}]
+        self.variable_status = "declaration"
 
-def p_start(p):
-    ''' Start : Strategy 
-    '''
-    print "End of Strategy!"
-    p[0] = p[1]
-    v = Visitor()
-    p[0].Visit(v)
-    v.GenerateCode ()
+    def p_start(p):
+        ''' Start : Strategy 
+        '''
+        print "End of Strategy!"
+        p[0] = p[1]
+        v = Visitor()
+        p[0].Visit(v)
+        v.GenerateCode ()
 
 
-# Start symbol: strategy
-def p_strategy(p):
-    ''' Strategy : Plan
-                 | Plan Strategy
-    '''
-    if len(p)==2:
-        p[0] = Strategy(uChildren=[ p[1] ] )
-    else:
-        p[0] = Strategy(uChildren=[ p[1], p[2] ])
-        
-    p[0]._children = flatten_strategies (p[0])
+    # Start symbol: strategy
+    def p_strategy(p):
+        ''' Strategy : Plan
+                     | Plan Strategy
+        '''
+        if len(p)==2:
+            p[0] = Strategy(uChildren=[ p[1] ] )
+        else:
+            p[0] = Strategy(uChildren=[ p[1], p[2] ])    
+        p[0]._children = flatten_strategies (p[0])
     
 
 # Plan
-def p_plan(p):
-    ''' Plan    : '(' seen_Plan Head ')' RANGLES '[' seen_IntentionList IntentionList ']' 
-    '''
-    global plan_count
-    plan_count+=1
-    print 'Successfully parsed Plan n. ' + str(plan_count)
-    p[8]._children = flatten_c(p[8]) 
-    p[0] = Plan(uChildren=[ p[3], p[8] ])
-    global sym_table_stack
-    sym_table_stack.pop()
+    def p_plan(p):
+        ''' Plan    : '(' seen_Plan Head ')' RANGLES '[' seen_IntentionList IntentionList ']' 
+        '''
+        self.plan_count += 1
+        print 'Successfully parsed Plan n. ' + str(self.plan_count)
+        p[8]._children = flatten_c(p[8]) 
+        p[0] = Plan(uChildren=[ p[3], p[8] ])
+        self.sym_table_stack.pop()
 
 
-def p_seen_IntentionList(p):
-    ''' seen_IntentionList : '''
-    global variable_status 
-    variable_status = "usage"
+        def p_seen_IntentionList(p):
+            ''' seen_IntentionList : '''
+            self.variable_status = "usage"
 
-
-
-def p_seen_Plan(p):
-    '''seen_Plan : '''
-    global sym_table_stack
-    sym_table_stack.append({})
-
-# Head
-def p_head(p):
-    ''' Head    : Event
-                | Event '|' '(' Condition ')'  
-    '''
+        def p_seen_Plan(p):
+            '''seen_Plan : '''
+            self.sym_table_stack.append({})
+            
+        # Head
+        def p_head(p):
+            ''' Head    : Event
+            | Event '|' '(' Condition ')'  
+                '''
     #print p.lineno(0)
-    p[1] = Trigger(uChildren=[p[1]])
-    if len(p)==6:
-        p[4]._children = flatten_c(p[4])
-        p[0] = Head(uChildren=[ p[1], p[4] ])
-    else:
-        p[0] = Head(uChildren=[p[1]])
+            p[1] = Trigger(uChildren=[p[1]])
+            if len(p)==6:
+                p[4]._children = flatten_c(p[4])
+                p[0] = Head(uChildren=[ p[1], p[4] ])
+            else:
+                p[0] = Head(uChildren=[p[1]])
         
-# Tiggering Event
-def p_event(p):
-    ''' Event   : GoalEvent
-                | BeliefEvent
-    '''
-    p[0] = p[1]
-# Goal Event
+        # Tiggering Event
+        def p_event(p):
+            ''' Event   : GoalEvent
+            | BeliefEvent
+            '''
+            p[0] = p[1]
 
-def p_goal_event(p):
-    ''' GoalEvent   : '+' Goal
-                    | '-' Goal
-    '''
-    p[0] = p[2]
+        # Goal Event
+        def p_goal_event(p):
+            ''' GoalEvent   : '+' Goal
+            | '-' Goal
+            '''
+            p[0] = p[2]
     #outputString = ""
     #outputString += "\nclass " + p[0] + "(Goal):\n\tpass"
     #print outputString
-#    print "Goal: " + p[0]
+    #    print "Goal: " + p[0]
 
-# Belief Event
-def p_belief_event(p):
-    ''' BeliefEvent : '+' Belief
-                    | '-' Belief
-    '''
-    p[0] = p[2]
+        # Belief Event
+        def p_belief_event(p):
+            ''' BeliefEvent : '+' Belief
+            | '-' Belief
+            '''
+            p[0] = p[2]
     #outputString = ""
     #outputString += "\nclass " + p[0] + "(Belief):\n\tpass"
     #print outputString
@@ -112,90 +106,89 @@ def p_belief_event(p):
 
 
 # Condition of Plan
-def p_condition(p):
-    ''' Condition   : Belief
-                    | Belief '&' Condition
-                    | '(' seen_LambdaExpr LambdaExpr ')'
-    '''
-    if len(p)==2:
-        p[0] = Condition(uChildren=[ p[1] ])
-    elif p[2]=='&':
-        p[0] = Condition(uChildren=[ p[1], p[3] ])
-    else:
-        p[0] = Condition(uChildren=[ p[2] ] )
+        def p_condition(p):
+            ''' Condition   : Belief
+            | Belief '&' Condition
+            | '(' seen_LambdaExpr LambdaExpr ')'
+            '''
+            if len(p)==2:
+                p[0] = Condition(uChildren=[ p[1] ])
+            elif p[2]=='&':
+                p[0] = Condition(uChildren=[ p[1], p[3] ])
+            else:
+                p[0] = Condition(uChildren=[ p[2] ] )
 
 
-def p_seen_LambdaExpr(p):
-    ''' seen_LambdaExpr : '''
-    global variable_status
-    variable_status = "usage"
+        def p_seen_LambdaExpr(p):
+            ''' seen_LambdaExpr : '''
+            self.variable_status = "usage"
 
 
 
 
 #eErrors
 # ----------------------------------------------------------
-def p_error_condition_with_event(p):
-    ''' Condition   : Event
-                    | Event '&' Condition
-    '''
-    mex = "Condition cannot contain Event"
-    print_error(mex, p.lineno(1))
+# def p_error_condition_with_event(p):
+#     ''' Condition   : Event
+#                     | Event '&' Condition
+#     '''
+#     mex = "Condition cannot contain Event"
+#     print_error(mex, p.lineno(1))
 
-def p_error_condition_with_goal(p):
-    ''' Condition   : Goal
-                    | Goal '&' Condition
-    '''
-    mex = "Condition cannot contain Goal"
-    print_error(mex, p.lineno(1))
+# def p_error_condition_with_goal(p):
+#     ''' Condition   : Goal
+#                     | Goal '&' Condition
+#     '''
+#     mex = "Condition cannot contain Goal"
+#     print_error(mex, p.lineno(1))
 
-def p_error_condition_lambda_not_enclosed(p):
-    ''' Condition   : LambdaExpr
-    '''
-    mex = "lambda is not enclosed in '(' ')'"
-    print_error(mex, p.lineno(1))
+# def p_error_condition_lambda_not_enclosed(p):
+#     ''' Condition   : LambdaExpr
+#     '''
+#     mex = "lambda is not enclosed in '(' ')'"
+#     print_error(mex, p.lineno(1))
 # ----------------------------------------------------------
 
 
 
-# Items in the Body of a Plan
-def p_intention_list(p):
-    ''' IntentionList   : Intention ',' IntentionList
-                        | Intention
-                        | empty 
-    '''
-    if len(p) == 4:
-        p[0] = Body(uChildren=[ p[1], p[3] ] )
-    elif p[1] != "empty":
-        p[0] = Body(uChildren=[ p[1] ] )
-    else:
-        p[0] = Body()
+    # Items in the Body of a Plan
+    def p_intention_list(p):
+        ''' IntentionList   : Intention ',' IntentionList
+        | Intention
+        | empty 
+        '''
+        if len(p) == 4:
+            p[0] = Body(uChildren=[ p[1], p[3] ] )
+        elif p[1] != "empty":
+            p[0] = Body(uChildren=[ p[1] ] )
+        else:
+            p[0] = Body()
 
 
+    # Single item in the body of a Plan
+    def p_intention(p):
+        ''' Intention   : Event
+        | AtomicAction
+        '''
+        p[0] = p[1]
 
-# Single item in the body of a Plan
-def p_intention(p):
-    ''' Intention   : Event
-                    | AtomicAction
-    '''
-    p[0] = p[1]
-# Lambda expression
-# es.1  lambda : Z>2
-def p_lambda_expr(p):
-    ''' LambdaExpr  : LAMBDA ':' LambdaTest
-    '''
-#    print "Lambda Parsed"
-    p[0] = Lambda()
+        # Lambda expression
+        # es.1  lambda : Z>2
+    def p_lambda_expr(p):
+        ''' LambdaExpr  : LAMBDA ':' LambdaTest
+        '''
+        #    print "Lambda Parsed"
+        p[0] = Lambda()
 
-# Condition to be tested in the lambda expression. 
-# es.2 lambda : (X!=2) and (Y<3) or (Z>=W) 
-def p_lambda_test(p):
-    ''' LambdaTest  : Comparison                    
-                    | CompoundTest
-    '''
-                   # | '(' LambdaTest ')'
+        # Condition to be tested in the lambda expression. 
+        # es.2 lambda : (X!=2) and (Y<3) or (Z>=W) 
+    def p_lambda_test(p):
+            ''' LambdaTest  : Comparison                    
+            | CompoundTest
+            '''
+# | '(' LambdaTest ')'
 
-def p_compound_test(p):
+        def p_compound_test(p):
     ''' CompoundTest    : UnaryTest ORLITERAL CompoundTest
                         | UnaryTest ANDLITERAL CompoundTest
                         | UnaryTest
