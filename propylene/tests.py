77183797cdc73f8eab79bf1940a03bbce31eaebc
loris <loris@kant.(none)>
2011-04-20 18:58:58 +0200

    added unit test

---
+++ propylene/tests.py
@@ -0,0 +1,28 @@
+from propylene import Propylene as Propylene
+from propylene import AugmentedPropylene as AugmentedPropylene
+import glob
+import unittest
+
+TEST_DIR = "test/"
+
+class PropyleneTest(unittest.TestCase):
+
+    def scenarios (self):
+        return glob.glob (TEST_DIR + "*-test")
+
+    def test_all (self):
+        p = AugmentedPropylene ()
+        scenarios = self.scenarios ()
+
+        for scenario in scenarios:
+            inputFile = open(scenario, 'r')
+            inputFileLines = inputFile.readlines()
+            inputFile.close()
+            totalString = "" 
+            for line in inputFileLines:
+                totalString += line
+                
+            result = p.parse(totalString)
+
+if __name__ == '__main__':
+    unittest.main()
