a235b0f022c788ee2f498fdbbeffe68adba211b0
loris <loris@kant.(none)>
2011-04-22 01:49:45 +0200

    added error tests to unit test

--- propylene/tests.py
+++ propylene/tests.py
@@ -9,6 +9,7 @@
 ##------------------------------------------------------------------------------
 from propylene import Propylene as Propylene
 from propylene import AugmentedPropylene as AugmentedPropylene
+from exception import *
 ##
 ##------------------------------------------------------------------------------
 ## global imports
@@ -24,10 +25,50 @@ TEST_DIR = "test/"
 class PropyleneTest(unittest.TestCase):
         
     def scenarios (self):
-        return { 'base' : ("classes/base-test-out.py", "test/base-test"),
-                 'cond' : ("classes/cond-test-out.py", "test/cond-test"),
-                 'args' : ("classes/args-test-out.py", "test/args-test"),
-                 'lambda' : ("classes/lambda-test-out.py", "test/lambda-test")}
+        return { 'base'     : ("classes/base-test-out.py", "test/base-test"),
+                 'cond'     : ("classes/cond-test-out.py", "test/cond-test"),
+                 'args'     : ("classes/args-test-out.py", "test/args-test"),
+                 'lambda'   : ("classes/lambda-test-out.py", "test/lambda-test"),
+                 'type-err' : ("classes/attitude-type-mismatch-err-out.py", "test/type-err"),
+                 'lex-err'  : ("classes/lexical-err-out.py", "test/lexical-err"),
+                 'prod-err' : ("classes/production-err-out.py", "test/production-err"),
+                 'syn-err'  : ("classes/syntax-err-out.py", "test/syntax-err"),
+                 'unb-err'  : ("classes/unbound-err-out.py", "test/unbound-err"),
+                 }
+
+    def test_attitude_type_mismatch (self):
+        scenario = self.scenarios ()['type-err']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.assertRaises (AttitudeTypeMismatch, 
+                           self.parse, inputfile, outputfile)
+
+    ## this test won't fail
+    def test_lexical_error (self):
+        scenario = self.scenarios ()['lex-err']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.parse (inputfile, outputfile)
+        
+    # ## this test fails, but raises an unexpected exception
+    def test_production_error (self):
+        scenario = self.scenarios ()['prod-err']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.parse (inputfile, outputfile)
+
+    def test_syntax_error (self):
+        scenario = self.scenarios ()['syn-err']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.parse (inputfile, outputfile)
+
+    def test_lexical_error (self):
+        scenario = self.scenarios ()['unb-err']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.assertRaises (UnboundedVariable, 
+                           self.parse, inputfile, outputfile)
 
     def test_lamb (self):
         scenario = self.scenarios ()['lambda']
