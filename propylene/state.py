e313bd2737c52244495183bd75d04c4ad91617c7
loris <loris@kant.(none)>
2011-04-21 19:25:10 +0200

    added the borg pattern for states

--- propylene/state.py
+++ propylene/state.py
@@ -1,9 +1,23 @@
+##------------------------------------------------------------------------------
+## state.py
+## 
+## This file contains the classes that encapsulates the variable parsing state
+## of Propylene
+##------------------------------------------------------------------------------
+## global imports
+##------------------------------------------------------------------------------
 from exception import *
-
+##
+##------------------------------------------------------------------------------
+## class VariableUsageState
+##
+## implements the 'Borg' pattern
+##------------------------------------------------------------------------------
 class VariableUsageState:
-    
+    __shared_state = {}    
+
     def __init__(self, *args, **kwargs):
-        pass
+        self.__dict__ = self.__shared_state
 
     def handle_symbol(self, symbol_table, symbol):
         ## if there are quotes, remove them
@@ -12,17 +26,19 @@ class VariableUsageState:
             symbol_table[symbol]
         except(KeyError):
             raise UnboundedVariable()
-
-
-
+##
+##------------------------------------------------------------------------------
+## class VariableDeclarationState
+##
+## implements the 'Borg' pattern
+##------------------------------------------------------------------------------
 class VariableDeclarationState:
+    __shared_state = {}    
     
     def __init__(self, *args, **kwargs):
-        pass
+        self.__dict__ = self.__shared_state
 
     def handle_symbol(self, symbol_table, symbol):
         ## if there are quotes, remove them
         if symbol[0] == "\"": symbol = symbol[1:-1]
-        symbol_table[symbol] = "Variable"        
-
-      
+        symbol_table[symbol] = "Variable"
