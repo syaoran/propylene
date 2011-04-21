457be29f075b43fed746f510a9011450a8aa20ac
loris <loris@kant.(none)>
2011-04-21 18:54:40 +0200

    unit test improved + different outputs for different tests

--- propylene/tests.py
+++ propylene/tests.py
@@ -11,23 +11,48 @@ import unittest
 TEST_DIR = "test/"
 
 class PropyleneTest(unittest.TestCase):
-
+        
     def scenarios (self):
-        return glob.glob (TEST_DIR + "*-test")
+        return { 'base' : ("classes/base-test-out.py", "test/base-test"),
+                 'cond' : ("classes/cond-test-out.py", "test/cond-test"),
+                 'args' : ("classes/args-test-out.py", "test/args-test"),
+                 'lamb' : ("classes/lamb-test-out.py", "test/lamb-test")}
 
-    def test_all (self):
-        p = AugmentedPropylene ()
-        scenarios = self.scenarios ()
+    def test_lamb (self):
+        scenario = self.scenarios ()['lamb']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.parse (inputfile, outputfile)
 
-        for scenario in scenarios:
-            inputFile = open(scenario, 'r')
-            inputFileLines = inputFile.readlines()
-            inputFile.close()
-            totalString = "" 
-            for line in inputFileLines:
-                totalString += line
-                
-            result = p.parse(totalString)
+    def test_args (self):
+        scenario = self.scenarios ()['args']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.parse (inputfile, outputfile)
+
+    def test_base (self):
+        scenario = self.scenarios ()['base']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.parse (inputfile, outputfile)
+
+    def test_cond (self):
+        scenario = self.scenarios ()['cond']
+        outputfile = scenario [0]
+        inputfile  = scenario [1]
+        self.parse (inputfile, outputfile)
+
+    def parse (self, uInputFile, uOutputFile):
+        p = AugmentedPropylene (out = uOutputFile)
+        inputFile = open (uInputFile, 'r')
+        inputFileLines = inputFile.readlines()
+        inputFile.close()
+        totalString = "" 
+        for line in inputFileLines:
+            totalString += line
+
+        result = p.parse (totalString)
+
 
 if __name__ == '__main__':
     unittest.main()
