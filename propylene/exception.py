5b9d3bbe099d6d9e07a1ddb568c7bb8e0e632d96
Syaoran <danielemar86@gmail.com>
2011-04-22 19:17:15 +0200

    - Refactored some productions to simplify the error handling; - Added support for Syntax error handling; - Some code cleaning in propylene.py - Merged the test file for syntax error in syntax-err - Deleted the now useless production-err - Updated the README and TODO

--- propylene/exception.py
+++ propylene/exception.py
@@ -3,12 +3,14 @@
 ###
 
 
-class SemanticError(Exception):
-    '''A semantic error has occurred'''
+class TooManySyntaxErrors(Exception):
+    ''' The syntax errors limit has been exceeded '''
 
+class SemanticError(Exception):
+    ''' A semantic error has occurred '''
 
 class AttitudeTypeMismatch(SemanticError):
-    ''' The same class refers to different attitude'''
+    ''' The same class refers to different attitudes '''
 
 class UnboundedVariable(SemanticError):
-    ''' Usage of an unbounded variable'''
+    ''' Usage of an unbounded Variable has been detected '''
--- propylene/propylene.py
+++ propylene/propylene.py
@@ -1,15 +1,16 @@
 ##------------------------------------------------------------------------------
 ## propylene.py
 ## 
-## This file contains the Grammar rules and the corresponding
+## This file contains the CFG specification of PROFETA and all necessary
 ## semantic actions
+
 ##------------------------------------------------------------------------------
 ## global imports
 ##------------------------------------------------------------------------------
 import sys
 import ply.lex as lex
 import ply.yacc as yacc
-##
+
 ##------------------------------------------------------------------------------
 ## local imports
 ##------------------------------------------------------------------------------
@@ -17,50 +18,73 @@ from prop_lexer import PropLexer as PropLexer
 from p_ast import *
 from exception import *
 from state import *
-##
+
 ##------------------------------------------------------------------------------
-## class Propylene
+## Propylene, the base class to build a PROFETA parser and class generator
 ##------------------------------------------------------------------------------
 class Propylene:
     def __init__ (self, *args, **kwargs):
+        ## DEBUG: keep track of the total number of plans
         self._plan_count = -1
-
-        self._sym_table_stack = [{}]
+        ## Total number of encountered syntax error
+        self._syntax_errors_count = 0
+        self._syntax_errors_info = []
+        ## If the following limit is exceeded, the parser stops
+        self._syntax_errors_limit = 10
+        ## The bottom of the stack is the global symbol table 
+        self._symbol_table_stack = [ {} ]
         self._variable_parsing_status = VariableDeclarationState()
-
+        
+        ## Build the lexer
         self._lexer = PropLexer()
+        ## Acquire the tokens defined and used by the lexer
         self.tokens = self._lexer.get_tokens()
+        ## Build the parser
+        self._parser = yacc.yacc(module=self, debug=1)
         #self._parser = yacc.yacc(module=self, tabmodule='parse_table', outputdir='parser_out')
-        self._parser = yacc.yacc(module=self,)
         
         ## if the output file was specified, save it
-        try: self._out = kwargs["out"]
-        except: pass
-        
-        
-    def parse(self, input):
-        return self._parser.parse(input, tracking=True)
-
+        try: 
+            self._out = kwargs["out"]
+        except: 
+            pass
     
+    ## Wrapper method for parsing
+    def parse(self, input):
+        ## PLY BUG! Ply can't track 'erro' special token
+        #return self._parser.parse(input, tracking=True)
+        return self._parser.parse(input)
+
+##------------------------------------------------------------------------------
+## CFG Grammar specification of PROFETA
+##------------------------------------------------------------------------------
+    # Start symbol of the grammar 
     def p_start(self, p):
         ''' Start : Strategy 
         '''
-        print "End of Strategy!"
-
-        ## create the visitor
-        try: v = Visitor(uTarget = self._out)
-        except: v = Visitor ()
-
-        p[0] = p[1]
+        #print "End of Strategy!"
         
-        ## visit the tree
-        p[0].Visit(v)
+        ## Syntax errors detected: cannot generate code
+        if self._syntax_errors_count>0:
+            print "Propylene has found {0} syntax errors:" .format(self._syntax_errors_count)
+            self.print_syntax_error_info()
+            print "Cannot generate code. Aborting."
+        else:
+            ## create the visitor
+            try: 
+                v = Visitor(uTarget = self._out)
+            except: 
+                v = Visitor ()
+            
+            p[0] = p[1]
+            
+            ## visit the tree
+            p[0].Visit(v)
 
-        ## generate the target
-        v.GenerateCode ()
+            ## generate the target
+            v.GenerateCode ()
 
-
-    # Start symbol: strategy
+    ## Strategy is a set of Plans
     def p_strategy(self, p):
         ''' Strategy : Plan
                      | Plan Strategy
@@ -72,50 +96,57 @@ class Propylene:
 
         p[0]._children = flatten_strategies (p[0])
     
-
-    # Plan
-    def p_plan(self, p):
-        ''' Plan    : '(' seen_Plan Head ')' RANGLES '[' seen_IntentionList IntentionList ']' 
+    ## a Plan in PROFETA
+    def p_plan(self, p): # >>
+        ''' Plan : Head RANGLES Body
         '''
         self._plan_count += 1
-        print 'Successfully parsed Plan n. ' + str(self._plan_count)
-        p[8]._children = flatten_c(p[8]) 
-        p[0] = Plan(uChildren=[ p[3], p[8] ])
-        self._sym_table_stack.pop()
+        print "Finished parsing Plan n. {0}" .format(self._plan_count)
+        # Build the 'Plan' Node
+        p[0] = Plan(uChildren=[ p[1], p[3] ])
+        # Pop the local symbol table
+        #print self._symbol_table_stack
+        self._symbol_table_stack.pop()
 
-    # An intentions list
-    def p_seen_IntentionList(self, p):
-        ''' seen_IntentionList : '''
+    # Head of a Plan, which contains the mandatory Triggering Event
+    # and an optional Condition
+    def p_head(self, p):
+        ''' Head : '(' enter_Head Event ')'
+                 | '(' enter_Head Event '|' Condition ')'
+        '''
+        p[3] = Trigger(uChildren=[ p[3] ])
+        if len(p)==5:   # Only the Triggering Event is specified
+            p[0] = Head(uChildren=[ p[3] ])
+        else:           # The Condition is specified, too
+            p[0] = Head(uChildren=[ p[3], p[5] ])
+    
+    # The Body of a plan is a sequence of intentions
+    def p_body(self, p):
+        ''' Body : '[' enter_Body IntentionList ']'  
+        '''
+        p[0] = p[3]
+        # flat the body
+        p[0]._children =  flatten_c(p[0])
+
+    # We just entered the head of a Plan, thus we create the
+    # local symbol table and shift to Declaration state
+    def p_enter_Head(self, p):
+        ''' enter_Head : '''
+        self._symbol_table_stack.append({})
+        self._variable_parsing_status = VariableDeclarationState ()
+ 
+    # We just entered the body of a Plan, and thus we must switch
+    # to Usage state
+    def p_enter_Body(self, p):
+        ''' enter_Body : '''
         self._variable_parsing_status = VariableUsageState()
 
-    ## Seen Plan (embedded action)
-    def p_seen_Plan(self, p):
-        '''seen_Plan : '''
-        self._sym_table_stack.append({})
-        self._variable_parsing_status = VariableDeclarationState ()
-        
-        
-    # Head
-    def p_head(self, p):
-        ''' Head    : Event
-                    | Event '|' '(' Condition ')'  
-            '''
-        p[1] = Trigger(uChildren=[p[1]])
-        if len(p)==6:
-            p[4]._children = flatten_c(p[4])
-            p[0] = Head(uChildren=[ p[1], p[4] ])
-            
-        else:
-            p[0] = Head(uChildren=[p[1]])
-            
-    
     # Triggering Event
     def p_event(self, p):
         ''' Event   : GoalEvent
                     | BeliefEvent
         '''
         p[0] = p[1]
-
 
     # Goal Event
     def p_goal_event(self, p):
@@ -124,7 +155,6 @@ class Propylene:
         '''
         p[0] = p[2]
 
-
     # Belief Event
     def p_belief_event(self, p):
         ''' BeliefEvent : '+' Belief
@@ -132,12 +162,18 @@ class Propylene:
         '''
         p[0] = p[2]
 
-
-    # Condition of Plan
+    # The condition of a plan is a sequence of beliefs with
+    # an optional lambda term
     def p_condition(self, p):
-        ''' Condition   : Belief
-                        | Belief '&' Condition
-                        | '(' seen_LambdaExpr LambdaExpr ')'
+        ''' Condition : '(' ConditionTerms ')'
+        '''
+        p[0] = p[2]
+        p[0]._children = flatten_c(p[0])
+
+    def p_condition_terms(self, p):
+        ''' ConditionTerms : Belief 
+                           | Belief '&' ConditionTerms
+                           | '(' enter_LambdaExpr LambdaExpr ')'
         '''
         if len(p)==2:
             p[0] = Condition(uChildren=[ p[1] ])
@@ -146,13 +182,11 @@ class Propylene:
         else:
             p[0] = Condition(uChildren=[ p[3] ] )
             
-
-    # Lambda Expr Seen (embedded action)
+    # We have just entered a lambda expression, which can only
+    # use previously bounded variables
     def p_seen_LambdaExpr(self, p):
-        ''' seen_LambdaExpr : '''
+        ''' enter_LambdaExpr : '''
         self._variable_parsing_status = VariableUsageState()
-        #"usage"
-
 
     # Items in the Body of a Plan
     def p_intention_list(self, p):
@@ -166,7 +200,6 @@ class Propylene:
             p[0] = Body(uChildren=[ p[1] ] )
         else:
             p[0] = Body()
-
 
     # An intention, i.e. a single item in the body of a Plan
     def p_intention(self, p):
@@ -183,33 +216,27 @@ class Propylene:
         '''
         p[0] = Lambda()
 
-
     # Condition to be tested in the lambda expression. 
     # e.g. 2 lambda : (X!=2) and (Y<3) or (Z>=W) 
     def p_lambda_test(self, p):
             ''' LambdaTest  : Comparison                    
                             | CompoundTest
             '''
-            # | '(' LambdaTest ')'
-
-
+    
     def p_compound_test(self, p):
         ''' CompoundTest    : UnaryTest ORLITERAL CompoundTest
                             | UnaryTest ANDLITERAL CompoundTest
                             | UnaryTest
         '''
-
     
     def p_unary_test(self, p):
         ''' UnaryTest   :   NOTLITERAL '(' Comparison ')'
                         |   '(' Comparison ')'
         '''
 
-    
     def p_comparison(self, p):
         ''' Comparison  :  CompTerm CompOp CompTerm '''
      
-    
     # Comparison term
     def p_comp_term(self, p):
         ''' CompTerm    : NAME
@@ -218,10 +245,8 @@ class Propylene:
         '''
         if isinstance(p[1], str):
             if p[1][0] != "\"":
-                self._variable_parsing_status.handle_symbol (self._sym_table_stack[-1], 
+                self._variable_parsing_status.handle_symbol (self._symbol_table_stack[-1], 
                                                              p[1])
-                
-                
     # Comparison operator
     def p_comp_op(self, p):
         ''' CompOp  : '>'  
@@ -233,14 +258,12 @@ class Propylene:
     
         '''
     
-
     # Belief
     def p_belief(self, p):
         ''' Belief  : NAME '(' ArgumentList ')'
         '''
         p[0] = Belief(uName=p[1])
         self.insert_symbol(p[1],'Belief')
-    
     
     # Goal
     def p_goal(self, p):
@@ -249,14 +272,13 @@ class Propylene:
         p[0] = Goal(uName=p[2])
         self.insert_symbol(p[2],'Goal')
     
-    
     # Action
     def p_atomicaction(self, p):
         ''' AtomicAction    : NAME '(' ArgumentList ')'
         '''
         p[0] = Action(uName=p[1])
         self.insert_symbol(p[1],'Action')
-        #print self._sym_table_stack
+        #print self._symbol_table_stack
         
     
     # List of arguments
@@ -266,7 +288,6 @@ class Propylene:
                             | empty
         '''
     
-
     # Single argument
     def p_argument(self, p):
         ''' Argument    : STRING
@@ -277,7 +298,7 @@ class Propylene:
                         | '(' ArgumentList ')'
         '''                 
         if len(p)==5:
-            self._variable_parsing_status.handle_symbol(self._sym_table_stack[-1], 
+            self._variable_parsing_status.handle_symbol(self._symbol_table_stack[-1], 
                                                         p[3])
                     
             
@@ -288,29 +309,45 @@ class Propylene:
         p[0] = "empty"
     
 
-    def p_error(self, p):
-        print 'Syntax error in input!'
-    
-    
-    def print_error(self, message, lineno):
-        print "Error (line " + str(lineno) + "): " + message
-    
+    # PLY error function
+    def p_error(self, p): 
+        # Simple and generic error message
+        self.add_generic_syntax_error_message(p)
+        ## 
+        #yacc.errok()
+        self._syntax_errors_count += 1
+        # Too many errors encountered: abort immediately
+        if self._syntax_errors_count>=self._syntax_errors_limit:
+            raise TooManySyntaxErrors()
+        #self.print_syntax_error_message(p)
+   
         
     ###
     # Ancillary Functions
     ###
     def insert_symbol(self, uName,uType):
-        #print self._sym_table_stack
+        #print self._symbol_table_stack
         try:
-            type = self._sym_table_stack[0][uName]
+            type = self._symbol_table_stack[0][uName]
             #print type, uType
             if type != uType:
                 raise AttitudeTypeMismatch()  
         except(KeyError):
-            self._sym_table_stack[0][uName] = uType
+            self._symbol_table_stack[0][uName] = uType
 
+    def add_generic_syntax_error_message(self, uLexToken):
+        mex = "Line {0} : Sintax error - unexpected token '{1}'"\
+                .format(uLexToken.lineno, uLexToken.value)
+        self._syntax_errors_info.append(mex)
 
-##------------------------------------------------------------------------------
+    def print_syntax_error_info(self):
+        for info in self._syntax_errors_info:
+            print info
+
+#    def print_syntax_error_message(self, uLexToken):
+#        print 
+        
+        ##------------------------------------------------------------------------------
 ## class AugmentedPropylene ---> Propylene
 ##------------------------------------------------------------------------------
 class AugmentedPropylene(Propylene):
@@ -318,27 +355,88 @@ class AugmentedPropylene(Propylene):
     def __init__(self, *args, **kwargs):
         Propylene.__init__(self, *args, **kwargs)
 
+
+    def add_custom_syntax_error_message(self, uMessage, uLexToken):
+        self._syntax_errors_info.append("Line {0} : {1}." \
+                                        .format(uLexToken.lineno, uMessage))
+ 
+
+
 # Error Productions
 # ----------------------------------------------------------
-    def p_error_condition_with_event(self, p):
-        ''' Condition   : Event
-                        | Event '&' Condition
+    
+    def p_head_error(self, p):
+        ''' Head : '(' enter_Head error ')'
         '''
-        mex = "Condition cannot contain Event"
-        self.print_error(mex, p.lineno(1))
-   
-    def p_error_condition_with_goal(self, p):
-        ''' Condition   : Goal
-                        | Goal '&' Condition
+        mex = "Error in the Head of the plan"
+        #print "Reducing..."
+        self.add_custom_syntax_error_message(mex, p[3])
+    
+    
+    # Error in Triggering Event
+    def p_trigger_error(self, p):
+        ''' Head : '(' enter_Head error '|' Condition ')'  
         '''
-        mex = "Condition cannot contain Goal"
-        self.print_error(mex, p.lineno(1))
+        mex = "Illegal Triggering Event"
+        self.add_custom_syntax_error_message(mex, p[3])
+    
 
-    def p_error_condition_lambda_not_enclosed(self, p):
-        ''' Condition   : LambdaExpr
+    # Error in Condition
+    def p_condition_error(self, p):
+        ''' Condition : '(' error ')'  
         '''
-        mex = "lambda is not enclosed in '(' ')'"
-        self.print_error(mex, p.lineno(1))
+        mex = "Error detected in the Condition of the plan"
+        self.add_custom_syntax_error_message(mex, p[2])
+ 
+
+    # Error in body
+    def p_body_error(self, p):
+        ''' Body : '[' enter_Body error ']'  
+        '''
+        mex = "Error detected in the Body of the plan"
+        self.add_custom_syntax_error_message(mex, p[3])
+ 
+#    # Error in Body
+#    def p_plan_body_error(self, p):
+#        ''' Plan    : '(' seen_Plan Head ')' RANGLES '[' seen_IntentionList error ']' 
+#        '''
+#        self._plan_count += 1
+#        print "Error in Body"
+#        self._symbol_table_stack.pop()
+#
+#   # lambda misspelled
+#    def p_lambda_expr_error(self, p):
+#        ''' LambdaExpr  : error ':' LambdaTest
+#        '''
+#        print "Syntax error: 'lambda' expected but", p[1].value, "found instead." #(lambda misspelled?)"
+#        raise SyntaxError
+#
+#   # Known error: an Event in Condition
+##    def p_error_condition_with_event(self, p):
+##        ''' Condition   : Event
+##                        | Event '&' Condition
+##        '''
+##        mex = "Condition cannot contain Event"
+##        self.print_error(mex, p.lineno(1))
+##        raise SyntaxError
+##    
+#    # Known error: a Goal in Condition
+#    def p_error_condition_with_goal(self, p):
+#        ''' Condition   : Goal
+#                        | Goal '&' Condition
+#        '''
+#        mex = "Condition cannot contain Goal"
+#        self.print_error(mex, p.lineno(1))
+#        raise SyntaxError
+#
+#    # Known error: a single lambda is not properly enclosed  
+#    def p_error_condition_lambda_not_enclosed(self, p):
+#        ''' Condition   : LambdaExpr
+#        '''
+#        mex = "lambda is not enclosed in '(' ')'"
+#        self.print_error(mex, p.lineno(1))
+#        raise SyntaxError
+#
 # ----------------------------------------------------------
 
 
